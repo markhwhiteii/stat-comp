b1b3df23af91fc898111b96e4fa67f241d1540a5
Mark White <markhwhiteii@gmail.com>
2017-10-30 21:49:30 -0500

    updated to match working.R output

--- R/working_analyses.R
+++ R/working_analyses.R
@@ -12,27 +12,24 @@ dat <- read_csv("data/results_df.csv") %>%
 
 glimpse(dat)
 
-## data characteristics not orthogonal
-dat[,c("precision", "recall", "f1", "auroc", "n", "noise_vars", 
-       "corr_vars", "linear_vars", "minority_size")] %>% 
-  cor(use = "pairwise.complete.obs")
+summary(dat)
+
+dat[,c("f1", "n", "noise_vars", "corr_vars", "linear_vars", "minority_size")] %>% 
+  cor(use = "pairwise.complete.obs") %>% 
+  round(3)
 
 ## compare by algorithm
-model_algorithm1 <- lm(auroc ~ algorithm, dat)
-summary(model_algorithm1) # xgboost does best
-model_algorithm2 <- lm(f1 ~ algorithm, dat)
-summary(model_algorithm2) # ada and xg do best
+model_algorithm <- lm(f1 ~ algorithm, dat)
+summary(model_algorithm)
 
 ## compare by sampling technique
-model_sampling1 <- lm(auroc ~ sampling, dat)
-summary(model_sampling1) # smote and under do best
-model_sampling2 <- lm(f1 ~ sampling, dat)
-summary(model_sampling2) # over and none do best?
+model_sampling <- lm(f1 ~ sampling, dat)
+summary(model_sampling)
 
 ## both together
 dat_summary <- dat %>% 
   group_by(algorithm, sampling) %>% 
-  summarise_at(vars(precision, recall, f1, auroc), funs(mean(., na.rm = TRUE)))
+  summarise_at(vars(precision, recall, f1), funs(mean(., na.rm = TRUE)))
 
 plot_precision <- ggplot(dat_summary, aes(x = algorithm, y = precision, fill = sampling)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -42,6 +39,6 @@ plot_recall <- ggplot(dat_summary, aes(x = algorithm, y = recall, fill = samplin
   geom_bar(stat = "identity", position = "dodge") +
   theme_light()
 
-plot_auroc <- ggplot(dat_summary, aes(x = algorithm, y = auroc, fill = sampling)) +
+plot_f1 <- ggplot(dat_summary, aes(x = algorithm, y = f1, fill = sampling)) +
   geom_bar(stat = "identity", position = "dodge") +
   theme_light()
