ae3ec79fcdb6828553c0dc36f0dc8f59eb1fc655
Mark White <markhwhiteii@gmail.com>
2017-11-02 11:09:38 -0500

    put other functions in other .R file

--- R/working.R
+++ R/working.R
@@ -1,74 +1,37 @@
-purrr::walk(
-  c("caret", "unbalanced", "randomForest", "fastAdaboost", 
-    "xgboost", "C50", "dplyr", "tidyr", "stringr", "readr"), 
-  library, character.only = TRUE
-)
-
-## prepare functions
-source("two_class_sim.R")
-
-get_results <- function(predicted, actual) {
-  conf_matrix <- as.data.frame(table(predicted, actual))
-  tp <- conf_matrix[(conf_matrix[1] == 1) & (conf_matrix[2] == 1), 3] %>% 
-    ifelse(length(.) == 0, 0, .)
-  fp <- conf_matrix[(conf_matrix[1] == 1) & (conf_matrix[2] == 0), 3] %>% 
-    ifelse(length(.) == 0, 0, .)
-  tn <- conf_matrix[(conf_matrix[1] == 0) & (conf_matrix[2] == 0), 3] %>% 
-    ifelse(length(.) == 0, 0, .)
-  fn <- conf_matrix[(conf_matrix[1] == 0) & (conf_matrix[2] == 1), 3] %>% 
-    ifelse(length(.) == 0, 0, .)
-  prec <- tp / (tp + fp)
-  rec <- tp / (tp + fn)
-  f1 <- 2 * ((prec * rec) / (prec + rec))
-  return(c(tp, fp, tn, fn, prec, rec, f1))
-}
-
-make_train <- function(dat, cases) {
-  train_X <- dat[cases, -ncol(dat)]
-  train_y <- as.factor(ifelse(dat[cases, ncol(dat)] == "Class2", 1, 0))
-  list(
-    train_under = ubUnder(train_X, train_y)[-3],
-    train_over = ubOver(train_X, train_y),
-    train_smote = ubSMOTE(train_X, train_y),
-    train_none = list(X = train_X, Y = train_y)
-  )
-}
-
-make_test <- function(dat, cases) {
-  list(
-    X = dat[-cases, -ncol(dat)], 
-    y = as.factor(ifelse(dat[-cases, ncol(dat)] == "Class2", 1, 0))
-  )
-}
+ps <- c("caret", "unbalanced", "randomForest", "fastAdaboost", 
+        "xgboost", "C50", "dplyr", "tidyr", "stringr", "readr")
+fs <- c("two_class_sim.R", "get_results.R", "make_train.R", "make_test.R")
+invisible(lapply(ps, library, character.only = TRUE))
+invisible(lapply(fs, source))
 
 ## set parameters
 set.seed(1839)
-n_iter <- 1000
-pred <- round(rnorm(n_iter, 50, 7))
-prop_noise <- abs(rnorm(n_iter, .15, .033))
-tot_noise <- round(pred * prop_noise)
-n <- round(rnorm(n_iter, 40000, 5000))
-noiseVars <- round(tot_noise / 2)
-corrVars <- tot_noise - noiseVars
-linearVars <- pred - tot_noise
+n_iter       <- 1000
+pred         <- round(rnorm(n_iter, 50, 7))
+prop_noise   <- abs(rnorm(n_iter, .15, .033))
+tot_noise    <- round(pred * prop_noise)
+n            <- round(rnorm(n_iter, 40000, 5000))
+noiseVars    <- round(tot_noise / 2)
+corrVars     <- tot_noise - noiseVars
+linearVars   <- pred - tot_noise
 minoritySize <- rnorm(n_iter, .03, .007)
-seeds <- as.integer(runif(n_iter, 1, 100000000))
+seeds        <- as.integer(runif(n_iter, 1, 100000000))
 
 results <- vector("list", 1600)
 for (iter in 1:100) {
   # generate data
   set.seed(seeds[iter])
   dat <- two_class_sim(
-    n = n[iter],
-    intercept = 0,
-    linearVars = linearVars[iter],
-    noiseVars = noiseVars[iter],
-    corrVars = corrVars[iter],
+    n            = n[iter],
+    intercept    = 0,
+    linearVars   = linearVars[iter],
+    noiseVars    = noiseVars[iter],
+    corrVars     = corrVars[iter],
     minoritySize = minoritySize[iter]
   )
-  cases <- sample(nrow(dat), nrow(dat) * .7)
-  training <- make_train(dat, cases)
-  testing <- make_test(dat, cases)
+  cases         <- sample(nrow(dat), nrow(dat) * .7)
+  training      <- make_train(dat, cases)
+  testing       <- make_test(dat, cases)
   minority_size <- prop.table(table(dat$Class))[[2]]
   
   counter <- 0
